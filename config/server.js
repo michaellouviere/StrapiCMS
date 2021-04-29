978f56b5bd18c9476e031ee46c0d3a6589c4f2e0
Mike Louviere <mike.louviere@hansoninc.com>
2021-04-29 12:33:48 -0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '312ea03827061d9606e760bfe592195f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5344e090-80ad-497e-be86-7f2a13460f2a'
+};
